6ed14849e30f4b4e8e601a336715c356d3d57018
yoshihiro_kato <yoshihiro_kato@katouyoshihironoMacBook-Pro.local>
2020-04-28 18:07:19 +0900

    フォローリストでログインユーザーにフォローボタンが表示されていたのを解消

--- src/resources/views/users/list.blade.php
+++ src/resources/views/users/list.blade.php
@@ -8,12 +8,14 @@
         @foreach($list as $user)
             <li class="list-group-item">
                 <a href="{{ route('user.show', $user->login_id) }}">{{ $user->name }}</a>
-                <div>
-                    <follow-button
-                        :initial-is-followed-by='@json($user->isFollowedBy(Auth::user()))'
-                        endpoint="{{ route('user.follow', ['login_id' => $user->login_id]) }}"
-                    ></follow-button>
-                </div>
+                @if($user->login_id !== Auth::user()->login_id)
+                    <div>
+                        <follow-button
+                            :initial-is-followed-by='@json($user->isFollowedBy(Auth::user()))'
+                            endpoint="{{ route('user.follow', ['login_id' => $user->login_id]) }}"
+                        ></follow-button>
+                    </div>
+                @endif
             </li>
         @endforeach
         </ul>
